29394f4d6aabc24e139537f86ff1d2be37f6f394
diego <diego@terminus.cl>
2024-07-11 13:27:52 -0400

    update fragment shader

--- src/shaders/fragment.glsl
+++ src/shaders/fragment.glsl
@@ -6,5 +6,18 @@ in vec3 vel;
 uniform vec3 color;
 
 void main() {
-  fragColor = vec4(vel, 1.0);
+
+  float MAX_SPEED = 25.f;
+  vec4 MAX_COLOR = vec4(1.0, 0.3, 0.1, 1.0);
+  vec4 MIN_COLOR = vec4(0.1, 0.3, 1.0, 1.0);
+
+  float mag = length(vel);
+  float val = mag/MAX_SPEED;
+  
+  if (val > 1) {
+    fragColor = MAX_COLOR;
+  }
+  else {
+    fragColor = mix(MIN_COLOR, MAX_COLOR, val);
+  }
 }
